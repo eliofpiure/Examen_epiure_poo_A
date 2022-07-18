4b4d996f1e042c7f23895bf5094d265c2e85640f
Usuario <Usuario@DESKTOP-M2UDKI8>
2022-07-18 11:50:22 -0500

    Esta es la prueba version 1.0

--- src/ec/edu/intsuperior/modelo/Cliente.java
+++ src/ec/edu/intsuperior/modelo/Cliente.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ec.edu.intsuperior.modelo;
 
-/**
- *
- * @author Usuario
- */
 public class Cliente {
-    
+
+    Persona persona = new Persona();
+    String telefono_contacto;
+
+    public String getTelefono_contacto() {
+        return telefono_contacto;
+    }
+
+    public void setTelefono_contacto(String telefono_contacto) {
+        this.telefono_contacto = telefono_contacto;
+    }
+
+    public String mostrar(String nombre, int edad, String telefonocontacto) {
+        return persona.mostrar(nombre, edad) + " " + telefono_contacto;
+    }
 }
